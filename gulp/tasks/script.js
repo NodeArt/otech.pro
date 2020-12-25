56acdab7ad86da77f5bb2310623d063615b24c15
Ivan Timoshenko <timoshenko.vanek@gmail.com>
2020-12-25 15:48:46 +0200

    fix tasks

--- gulp/tasks/script.js
+++ gulp/tasks/script.js
@@ -4,7 +4,7 @@ const rename = require('gulp-rename')
 
 module.exports = function script(cb) {
     return gulp.src('./src/js/main.js')
-        // .pipe(terser())
+        .pipe(terser())
         .pipe(gulp.dest('./dist/js'))
         .pipe(rename({
             suffix: '.min'
--- src/js/main.js
+++ src/js/main.js
@@ -33,20 +33,18 @@
             submitHandler: function() {
                 let form_data = $(this.currentForm).serializeArray();
                 $.ajax({
-                    url: 'sendMail.php',
+                    url: '/sendMail',
                     method: 'POST',
                     dataType: 'json',
-                    data: {
-                        data: form_data
-                    },
+                    data: form_data,
                     success: function (response) {
                         let text = '',
                             notify_class = '';
-                        if (response.success !== undefined) {
-                            text = response.success;
+                        if (response.data !== undefined) {
+                            text = 'Сообщение успешно отправлено!';
                             notify_class = 'success';
                         } else {
-                            text = response.error;
+                            text =  'Сообщение не отправлено!';
                             notify_class = 'error';
                         }
                         $('.form-contact').append('<div id="email_notify" class="'+ notify_class +'">'+ text +'!</div>')
@@ -59,75 +57,6 @@
                 })
             }
         });
-        // $(document).on('click touchstart tap','.custom-scroll .fl-button', function () {
-        //     $('html, body').animate({scrollTop: $(this).parents('.fl-row').next().position().top }, '500', 'swing');
-        //     return false;
-        // }).on('click touchstart tap','.fl-page-footer-wrap .custom-scroll-footer', function () {
-        //     $('html, body').animate({ scrollTop: 0 }, '500', 'swing');
-        //     return false;
-        // });
-
-
-        if ($(window).width() > 1024) {
-
-        }
-
-        // var slider = $('.block-heading__wrap');
-        //
-        // slider.slick({
-        //     infinite: false,
-        //     slidesToShow: 1,
-        //     slidesToScroll: 1,
-        //     dots: true,
-        //     arrows: false,
-        //     vertical: true,
-        //     verticalScrolling: true,
-        //     verticalSwiping: true,
-        //     customPaging : function(slider, i) {
-        //         return '0' + i;
-        //     },
-        // });
-        //
-        // slider.on('wheel', (function(e) {
-        //     e.preventDefault();
-        //
-        //     if (e.originalEvent.deltaY < 0) {
-        //         $(this).slick('slickPrev');
-        //     } else {
-        //         $(this).slick('slickNext');
-        //     }
-        // }));
-        //
-        // $('.menu li').each(function (idx, itm) {
-        //     $('a', itm).on('click', function (e) {
-        //         e.preventDefault();
-        //         $(this).parent().siblings().removeClass('active');
-        //         $(this).parent().addClass('active');
-        //         slider.slick('slickGoTo', idx);
-        //     })
-        // });
-        //
-        // slider.on('afterChange', function (event, slick, currentSlide) {
-        //     $('header').attr('class', '');
-        //     $('header').addClass($(slick.$slides[currentSlide]).attr('data-header-class'));
-        // });
-        //
-        //
-        // $(".on-main").on("click", function() {
-        //     slider.slick('slickGoTo', 0);
-        // });
-
-        $('[data-fancybox="images"]').fancybox({
-            // Options will go here
-        });
-        //
-        // $('a.open-popup-link').magnificPopup({
-        //     type:'inline',
-        //     midClick: true,
-        //     closeBtnInside:true
-        // });
-        //
-
 
         /** HEIGHT BLOCK **/
         function heightEL(el) {
@@ -225,24 +154,6 @@
         })
 
     });
-
-    // $(document)
-    // .mousemove(function(e) {
-    //     $('.cursor')
-    //         .eq(0)
-    //         .css({
-    //             transformX: e.clientX,
-    //             transformY: e.clientY
-    //         });
-    //     setTimeout(function() {
-    //         $('.cursor')
-    //             .eq(1)
-    //             .css({
-    //                 transformX: e.clientX,
-    //                 transformY: e.clientY,
-    //             });
-    //     }, 100);
-    // })
 
     $(".btn-toggle").on("click", function () {
         $(this).toggleClass("active");
