9a5adeca9fc27367a19d7c269492183ae4e159fe
Ivan Timoshenko <timoshenko.vanek@gmail.com>
2020-12-25 14:35:59 +0200

    Revert "code commit"
    This reverts commit 72dd6c2100742cdcbbc3d8c9f20349fb699651c1.

--- app/js/all-plugins.js
+++
@@ -1,6 +0,0 @@
-//= plugins/jquery-3.5.1.min.js
-//= plugins/slick.min.js
-//= plugins/jquery.fancybox.min.js
-//= plugins/select2.full.min.js
-//= plugins/jquery.magnific-popup.min.js
-//= plugins/jquery.validate.min.js
--- gulp/tasks/fonts.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp')
-
-module.exports = function fonts(cb) {
-    return gulp.src('app/fonts/*.*')
-        .pipe(gulp.dest('fonts/'));
-    return cb()
-};
